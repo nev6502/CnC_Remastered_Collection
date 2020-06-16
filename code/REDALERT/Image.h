19b6c46149b6f2463da6610007134f135d318b0a
Justin Marshall <justinmarshall20@gmail.com>
2020-06-16 07:25:15 -0700

    Added 32bit image(single frame) overrides for buildings. Image_t now stores raw pixel data.

--- code/REDALERT/Image.h
+++ code/REDALERT/Image.h
@@ -2,10 +2,25 @@
 //
 
 struct Image_t {
+	Image_t();
+	~Image_t();
+
 	char name[512];
 	unsigned int image;
 	int width;
 	int height;
+	unsigned char* buffer;
 };
+
+__forceinline Image_t::Image_t() {
+	buffer = NULL;
+}
+
+__forceinline Image_t::~Image_t() {
+	if (buffer) {
+		delete buffer;
+		buffer = NULL;
+	}
+}
 
 Image_t* Image_LoadImage(const char* name);
