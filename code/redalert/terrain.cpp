20a5c42f3ae365e02e31991b1ca09d9adc580151
Justin Marshall <justinmarshall20@gmail.com>
2020-07-02 12:40:46 -0700

    Terrain data can now be placed again.

--- code/redalert/terrain.cpp
+++ code/redalert/terrain.cpp
@@ -491,11 +491,11 @@ void TerrainClass::AI(void)
 	assert(IsActive);
 
 	ObjectClass::AI();
-// jmarshall - add back terrain mine
-	//if ((*this == TERRAIN_MINE) && (Frame % (Rule.GrowthRate * TICKS_PER_MINUTE)) == 0) {
-	//	Map[::As_Cell(As_Target())].Spread_Tiberium(true);
-	//}
-// jmarshall end
+
+	if ((*this == TERRAIN_MINE) && (Frame % (Rule.GrowthRate * TICKS_PER_MINUTE)) == 0) {
+		Map[::As_Cell(As_Target())].Spread_Tiberium(true);
+	}
+
 	if (StageClass::Graphic_Logic()) {
 		Mark();
 
